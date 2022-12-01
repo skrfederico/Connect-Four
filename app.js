0b86d51e03febaee0796c5164a99b9dae5af1b85
federico <skrfederico@gmail.com>
2022-12-01 02:44:49 -0800

    after connect 4 board cleans

--- app.js
+++ app.js
@@ -31,237 +31,161 @@ function setBoard() {
   document.getElementById('playArea').style.display = 'flex'
 }
 
-let matchNumber = 1
+// document.addEventListener('DOMContentLoaded', () => {
+const squares = document.querySelectorAll('.grid div')
+const result = document.getElementById('result')
+const displayCurrentPlayer = document.getElementById('current-player')
+const displayCurrentMatch = document.getElementById('current-match')
 
-function playAgain() {
-  document.getElementById('playArea').style.display = 'none'
-  document.getElementById(
-    'playArea'
-  ).innerHTML = `<div id="playArea" class="container">
-  <div id="score1" class="scoreboard">
-    <div><p>Finish line</p></div>
-    <ul id="p1column" class="sb-column">
-      <div id="sb1-col-row10"></div>
-      <div id="sb1-col-row9"></div>
-      <div id="sb1-col-row8"></div>
-      <div id="sb1-col-row7"></div>
-      <div id="sb1-col-row6"></div>
-      <div id="sb1-col-row5"></div>
-      <div id="sb1-col-row4"></div>
-      <div id="sb1-col-row3"></div>
-      <div id="sb1-col-row2"></div>
-      <div id="sb1-col-row1"></div>  
-      </ul>
-      <div><p>Start line</p></div>
-    </div>
-    
-    <div class="grid">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    <div class="bottom">Not here!</div>
-    </div>
-  <div id="score2" class="scoreboard">
-  <div><p>Finish line</p></div>
-  <ul id="p2column" class="sb-column">
-    <div id="sb2-col-row10"></div>
-    <div id="sb2-col-row9"></div>
-    <div id="sb2-col-row8"></div>
-    <div id="sb2-col-row7"></div>
-    <div id="sb2-col-row6"></div>
-    <div id="sb2-col-row5"></div>
-    <div id="sb2-col-row4"></div>
-    <div id="sb2-col-row3"></div>
-    <div id="sb2-col-row2"></div>
-    <div id="sb2-col-row1"></div>  
-    </ul>
-  <div><p>Start line</p></div>
-  </div>
-`
-  document.getElementById('playArea').style.display = 'flex'
+let currentPlayer = 1
+
+const winningCombinations = [
+  [0, 1, 2, 3],
+  [41, 40, 39, 38],
+  [7, 8, 9, 10],
+  [34, 33, 32, 31],
+  [14, 15, 16, 17],
+  [27, 26, 25, 24],
+  [21, 22, 23, 24],
+  [20, 19, 18, 17],
+  [28, 29, 30, 31],
+  [13, 12, 11, 10],
+  [35, 36, 37, 38],
+  [6, 5, 4, 3],
+  [0, 7, 14, 21],
+  [41, 34, 27, 20],
+  [1, 8, 15, 22],
+  [40, 33, 26, 19],
+  [2, 9, 16, 23],
+  [39, 32, 25, 18],
+  [3, 10, 17, 24],
+  [38, 31, 24, 17],
+  [4, 11, 18, 25],
+  [37, 30, 23, 16],
+  [5, 12, 19, 26],
+  [36, 29, 22, 15],
+  [6, 13, 20, 27],
+  [35, 28, 21, 14],
+  [0, 8, 16, 24],
+  [41, 33, 25, 17],
+  [7, 15, 23, 31],
+  [34, 26, 18, 10],
+  [14, 22, 30, 38],
+  [27, 19, 11, 3],
+  [35, 29, 23, 17],
+  [6, 12, 18, 24],
+  [28, 22, 16, 10],
+  [13, 19, 25, 31],
+  [21, 15, 9, 3],
+  [20, 26, 32, 38],
+  [36, 30, 24, 18],
+  [5, 11, 17, 23],
+  [37, 31, 25, 19],
+  [4, 10, 16, 22],
+  [2, 10, 18, 26],
+  [39, 31, 23, 15],
+  [1, 9, 17, 25],
+  [40, 32, 24, 16],
+  [9, 17, 25, 33],
+  [8, 16, 24, 32],
+  [11, 17, 23, 29],
+  [12, 18, 24, 30],
+  [1, 2, 3, 4],
+  [5, 4, 3, 2],
+  [8, 9, 10, 11],
+  [12, 11, 10, 9],
+  [15, 16, 17, 18],
+  [19, 18, 17, 16],
+  [22, 23, 24, 25],
+  [26, 25, 24, 23],
+  [29, 30, 31, 32],
+  [33, 32, 31, 30],
+  [36, 37, 38, 39],
+  [40, 39, 38, 37],
+  [7, 14, 21, 28],
+  [8, 15, 22, 29],
+  [9, 16, 23, 30],
+  [10, 17, 24, 31],
+  [11, 18, 25, 32],
+  [12, 19, 26, 33],
+  [13, 20, 27, 34]
+]
+
+//check winning combinations
+function checkWinner() {
+  for (let y = 0; y < winningCombinations.length; y++) {
+    const square1 = squares[winningCombinations[y][0]]
+    const square2 = squares[winningCombinations[y][1]]
+    const square3 = squares[winningCombinations[y][2]]
+    const square4 = squares[winningCombinations[y][3]]
+    //is player 1 the winner?
+    if (
+      square1.classList.contains('player1') &&
+      square2.classList.contains('player1') &&
+      square3.classList.contains('player1') &&
+      square4.classList.contains('player1')
+    ) {
+      result.innerHTML = `Player 1 wins`
+      // currentMatch = currentMatch + 1
+      // displayCurrentMatch.innerHTML = currentMatch
+      playAgain()
+    }
+    //is player 2 the winner?
+    if (
+      square1.classList.contains('player2') &&
+      square2.classList.contains('player2') &&
+      square3.classList.contains('player2') &&
+      square4.classList.contains('player2')
+    ) {
+      result.innerHTML = `Player 2 wins`
+      // currentMatch = currentMatch + 1
+      // displayCurrentMatch.innerHTML = currentMatch
+      playAgain()
+    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const squares = document.querySelectorAll('.grid div')
-  const result = document.getElementById('result')
-  const displayCurrentPlayer = document.getElementById('current-player')
-  const displayCurrentMatch = document.getElementById('current-match')
-
-  let currentPlayer = 1
-
-  const winningCombinations = [
-    [0, 1, 2, 3],
-    [41, 40, 39, 38],
-    [7, 8, 9, 10],
-    [34, 33, 32, 31],
-    [14, 15, 16, 17],
-    [27, 26, 25, 24],
-    [21, 22, 23, 24],
-    [20, 19, 18, 17],
-    [28, 29, 30, 31],
-    [13, 12, 11, 10],
-    [35, 36, 37, 38],
-    [6, 5, 4, 3],
-    [0, 7, 14, 21],
-    [41, 34, 27, 20],
-    [1, 8, 15, 22],
-    [40, 33, 26, 19],
-    [2, 9, 16, 23],
-    [39, 32, 25, 18],
-    [3, 10, 17, 24],
-    [38, 31, 24, 17],
-    [4, 11, 18, 25],
-    [37, 30, 23, 16],
-    [5, 12, 19, 26],
-    [36, 29, 22, 15],
-    [6, 13, 20, 27],
-    [35, 28, 21, 14],
-    [0, 8, 16, 24],
-    [41, 33, 25, 17],
-    [7, 15, 23, 31],
-    [34, 26, 18, 10],
-    [14, 22, 30, 38],
-    [27, 19, 11, 3],
-    [35, 29, 23, 17],
-    [6, 12, 18, 24],
-    [28, 22, 16, 10],
-    [13, 19, 25, 31],
-    [21, 15, 9, 3],
-    [20, 26, 32, 38],
-    [36, 30, 24, 18],
-    [5, 11, 17, 23],
-    [37, 31, 25, 19],
-    [4, 10, 16, 22],
-    [2, 10, 18, 26],
-    [39, 31, 23, 15],
-    [1, 9, 17, 25],
-    [40, 32, 24, 16],
-    [9, 17, 25, 33],
-    [8, 16, 24, 32],
-    [11, 17, 23, 29],
-    [12, 18, 24, 30],
-    [1, 2, 3, 4],
-    [5, 4, 3, 2],
-    [8, 9, 10, 11],
-    [12, 11, 10, 9],
-    [15, 16, 17, 18],
-    [19, 18, 17, 16],
-    [22, 23, 24, 25],
-    [26, 25, 24, 23],
-    [29, 30, 31, 32],
-    [33, 32, 31, 30],
-    [36, 37, 38, 39],
-    [40, 39, 38, 37],
-    [7, 14, 21, 28],
-    [8, 15, 22, 29],
-    [9, 16, 23, 30],
-    [10, 17, 24, 31],
-    [11, 18, 25, 32],
-    [12, 19, 26, 33],
-    [13, 20, 27, 34]
-  ]
-
-  //check winning combinations
-  function checkWinner() {
-    for (let y = 0; y < winningCombinations.length; y++) {
-      const square1 = squares[winningCombinations[y][0]]
-      const square2 = squares[winningCombinations[y][1]]
-      const square3 = squares[winningCombinations[y][2]]
-      const square4 = squares[winningCombinations[y][3]]
-      //is player 1 the winner?
-      if (
-        square1.classList.contains('player1') &&
-        square2.classList.contains('player1') &&
-        square3.classList.contains('player1') &&
-        square4.classList.contains('player1')
-      ) {
-        result.innerHTML = `Player 1 wins`
-        // currentMatch = currentMatch + 1
-        // displayCurrentMatch.innerHTML = currentMatch
-        playAgain()
+for (let i = 0; i < squares.length; i++) {
+  squares[i].onclick = () => {
+    squares[i].setAttribute('id', i)
+    // alert(`You've clicked square ` + i)
+    // console.log(squares)
+    if (
+      squares[i + 7].classList.contains('bottom') &&
+      !squares[i].classList.contains('bottom')
+    ) {
+      if (currentPlayer == 1) {
+        squares[i].classList.add('bottom')
+        squares[i].classList.add('player1')
+        currentPlayer = 2
+        displayCurrentPlayer.innerHTML = currentPlayer
+      } else if (currentPlayer == 2) {
+        squares[i].classList.add('bottom')
+        squares[i].classList.add('player2')
+        currentPlayer = 1
+        displayCurrentPlayer.innerHTML = currentPlayer
       }
-      //is player 2 the winner?
-      if (
-        square1.classList.contains('player2') &&
-        square2.classList.contains('player2') &&
-        square3.classList.contains('player2') &&
-        square4.classList.contains('player2')
-      ) {
-        result.innerHTML = `Player 2 wins`
-        currentMatch = currentMatch + 1
-        displayCurrentMatch.innerHTML = currentMatch
-        playAgain()
-      }
-    }
+    } else alert('invalid move')
+    checkWinner()
   }
+}
 
+let matchNumber = 1
+
+function eraseClasses() {
+  for (let i = 0; i < 41; i++) {
+    squares[i].classList.remove('bottom')
+  }
+}
+
+function playAgain() {
   for (let i = 0; i < squares.length; i++) {
-    squares[i].onclick = () => {
-      squares[i].setAttribute('id', i)
-      // alert(`You've clicked square ` + i)
-      // console.log(squares)
-      if (
-        squares[i + 7].classList.contains('bottom') &&
-        !squares[i].classList.contains('bottom')
-      ) {
-        if (currentPlayer == 1) {
-          squares[i].classList.add('bottom')
-          squares[i].classList.add('player1')
-          currentPlayer = 2
-          displayCurrentPlayer.innerHTML = currentPlayer
-        } else if (currentPlayer == 2) {
-          squares[i].classList.add('bottom')
-          squares[i].classList.add('player2')
-          currentPlayer = 1
-          displayCurrentPlayer.innerHTML = currentPlayer
-        }
-      } else alert('invalid move')
-      checkWinner()
-    }
+    // squares[i].classList.add('bottom')
+    squares[i].classList.remove('player1')
+    squares[i].classList.remove('player2')
+    currentPlayer = 1
+    eraseClasses()
   }
-})
+}
+
+// })
