25c57ffa49142a3add0fa0c1fe3a9622df7a9729
federico <skrfederico@gmail.com>
2022-12-03 13:42:12 -0800

    pseudo code for winning conditions

--- app.js
+++ app.js
@@ -50,70 +50,45 @@ let currentPlayer = playerX
 
 let matchNumber = 1
 
-// function eraseClasses() {
-//   for (let i = 0; i < 41; i++) {
-//     squares[i].classList.remove('bottom')
-//   }
-// }
-
 class GameBoard {
   constructor(board) {
     this.board = board
   }
 
+  // INSIDE GAME LOGIC
   placeToken(player1, x) {
     this.board[x][y] = player1
     this.checkForWinner()
   }
 
   refreshBoard() {
-    // Loop over this.board and add the appropriate class to the correct
-    // cell in the dom
-
-    //MY CODE BELOW
-    function eraseClasses() {
-      for (let i = 0; i < 41; i++) {
-        board[i][j].classList.remove('bottom')
-      }
-    }
+    //MY CODE: ARROW FUNCTION INSIDE GAME LOGIC
     //how to loop in a 2d array
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
         board[i][j].classList.remove('xSquare')
         board[i][j].classList.remove('oSquare')
         board[i][j].innerText = ''
-        eraseClasses()
       }
     }
   }
 
   checkForWinner() {
-    // /   for (let r = 0; r < example.length; r++) {
-    //     for (let c = 0; c < example[r].length; c++) {
-    //       const node = example[r][c];
-    //       const coords = {
-    //         x: r,
-    //         y: c,
-    //       };
-    //       console.log(node, coords);
-    //     }
-    //   }
-    // if (
-    //   squares[i + 7].classList.contains('bottom') &&
-    //   !squares[i].classList.contains('bottom')
-    // ) {
-    //   if (currentPlayer == 1) {
-    //     squares[i].classList.add('bottom')
-    //     squares[i].classList.add('player1')
-    //     currentPlayer = 2
-    //     displayCurrentPlayer.innerHTML = currentPlayer
-    //   } else if (currentPlayer == 2) {
-    //     squares[i].classList.add('bottom')
-    //     squares[i].classList.add('player2')
-    //     currentPlayer = 1
-    //     displayCurrentPlayer.innerHTML = currentPlayer
-    //   }
-    // } else alert('invalid move')
+    for (let r = 0; r < squares.length; r++) {
+      for (let c = 0; c < subarray.length; c++) {
+        const node = subarray[c]
+        const coords = {
+          x: r,
+          y: c
+        }
+        console.log(node, coords)
+      }
+    }
+  }
+
+  checkForConnectFour() {
+    checkForHorPosLine()
+    checkForHorNegLine()
   }
 }
 /// this is the end of the class
@@ -121,15 +96,13 @@ class GameBoard {
 let connectFour = new GameBoard(squares)
 
 let board = connectFour.board
-console.log(board)
+// console.log(board)
 
-//how to loop in a 2d array
-// GAMEPLAY function
+// GAME LOGIC
 for (let i = 0; i < squares.length; i++) {
   let subarray = squares[i].children
   for (let j = 0; j < subarray.length; j++) {
-    console.log(subarray[j].tagName)
-
+    // console.log(subarray[j].tagName)
     subarray[j].addEventListener('click', () => {
       if (squares[i].id === 'row5') {
         if (currentPlayer === playerX) {
@@ -156,6 +129,8 @@ for (let i = 0; i < squares.length; i++) {
         subarray[j].innerHTML = currentPlayer
         currentPlayer = playerX
       } else alert(`invalid move`)
+
+      // checkForConnectFour()
     })
 
     const playAgain = () => {
@@ -163,68 +138,10 @@ for (let i = 0; i < squares.length; i++) {
       subarray[j].classList.remove('xSquare')
       subarray[j].classList.remove('oSquare')
       subarray[j].innerText = ''
-      // eraseClasses()
     }
     reset.addEventListener('click', playAgain)
   }
 }
-
-// // function playAgain() {
-// const playAgain = () => {
-//   console.log('reset clicked')
-//   for (let i = 0; i < board.length; i++) {
-//     for (let j = 0; j < subarray.length; j++) {
-//       subarray[j].classList.remove('xSquare')
-//       subarray[j].classList.remove('oSquare')
-//       subarray[j].innerText = ''
-//       // eraseClasses()
-//     }
-//   }
-// }
-// reset.addEventListener('click', playAgain)
-
-// debugger
-// for (let i = 0; i < squares.length; i++) {
-//   squares[i].addEventListener('click', () => {
-//     // changeTurns(i)
-//     if (
-//       squares[i + 7].classList.contains('bottom') &&
-//       !squares[i].classList.contains('bottom') &&
-//       squares[i].innerText === ''
-//     ) {
-//       if (currentPlayer == playerX) {
-//         squares[i].classList.add('bottom')
-//         squares[i].classList.add('xSquare')
-//         squares[i].innerHTML = currentPlayer
-//         // board.board[i][i] = currentPlayer
-//         currentPlayer = playerO
-//         displayCurrentPlayer.innerHTML = currentPlayer
-//       } else if (currentPlayer == playerO) {
-//         squares[i].classList.add('bottom')
-//         squares[i].classList.add('oSquare')
-//         squares[i].innerHTML = currentPlayer
-//         // board[num][num] = currentPlayer
-//         currentPlayer = playerX
-//         displayCurrentPlayer.innerHTML = currentPlayer
-//       }
-//     } else alert(`invalid move`)
-//   })
-// }
-
-// // changeTurns function
-// function changeTurns(num) {
-//   if (squares[num].innerText === '' && currentPlayer) {
-//     squares[num].innerText = currentPlayer
-//     if (currentPlayer === 'X') {
-//       squares[num].classList.add('xSquare')
-//       currentPlayer = 'O'
-//     } else {
-//       squares[num].classList.add('oSquare')
-//       currentPlayer = 'X'
-//     }
-//     // checkForWinner()
-//   }
-// }
 
 // RECURSION
 // function checkWin(n)
@@ -245,6 +162,40 @@ for (let i = 0; i < squares.length; i++) {
 //     3! = 3 * 2 * 1 = 6
 //     6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
 // */
+
+//PSEUDO CODE FOR WINNING COMBINATIONS
+function checkForHorPosLine(n) {
+  // exit condition
+  if ((n = subarray[j + 3].classList.contains('playerX')))
+    // || if(n = (subarray[j+3].innerHTML = playerX))
+    return
+  checkForHorPosLine(subarray[j + 1].classList.contains('playerX'))
+  // || checkForHorPosLine((subarray[j+1]).innerHTML = playerX)
+}
+
+function checkForHorNegLine(n) {
+  // exit condition
+  if ((n = subarray[j - 3].classList.contains('playerX')))
+    // || if(n = (subarray[j+3].innerHTML = playerX))
+    return
+  checkForHorNegLine(subarray[j - 1].classList.contains('playerX'))
+  // || checkForHorNegLine((subarray[j-1]).innerHTML = playerX)
+}
+
+// HORIZONTAL WIN CHECK
+//squareClicked, subarray[j+1], subarray[j+2], subarray[j+3]
+//squareClicked, subarray[j-1], subarray[j-2], subarray[j-3]
+
+// VERTICAL WIN CHECK
+//squareClicked, squares[i + 1].children[j], squares[i + 2].children[j], squares[i + 3].children[j]
+
+// DIAGONAL WIN CHECK (TOP LEFT - BOTTOM RIGHT)
+//squareClicked, squares[i + 1].children[j+1], squares[i + 2].children[j+2], squares[i + 3].children[j+3]
+//squareClicked, squares[i - 1].children[j-1], squares[i - 2].children[j-2], squares[i - 3].children[j-3]
+
+// DIAGONAL WIN CHECK (BOTTOM LEFT - TOP RIGHT)
+//squareClicked, squares[i - 1].children[j+1], squares[i - 2].children[j+2], squares[i - 3].children[j+3]
+//squareClicked, squares[i + 1].children[j-1], squares[i + 2].children[j-2], squares[i + 3].children[j-3]
 
 // function factorial(num) {
 //     if(num < 1 || num % 1 !== 0) return "Please enter an integer that's more than 1"
